1041f402cbd154064aacb74c6cd6159189d31ead
Mohammad Fayed <MFayed@healthinsightsgroup.com>
2024-04-23 17:33:38 +0200

    push session 1 presentation and demo code.

---
+++ Session 1/CPM/CPM.BusinessLogic/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPM.BusinessLogic
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                string fullName = FirstName;
+                if (!string.IsNullOrWhiteSpace(LastName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += LastName;
+                }
+                return fullName;
+            }
+        }
+        public string Email { get; set; }
+    }
+}
---
+++ Session 1/CPM/CPM.UnitTest/CustomerTest.cs
@@ -0,0 +1,41 @@
+﻿using CPM.BusinessLogic;
+
+namespace CPM.UnitTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void Fullname_ValidFullName_commaSeparatedFirstNameAndLastName()
+        {
+            // arrange
+            var customer = new Customer
+            {
+                CustomerId = 1,
+                FirstName = "Mohammad",
+                LastName = "Atef",
+            };
+
+            // act
+            var acutal = "Mohammad, Atef";
+
+            // assert
+            Assert.Equal(acutal, customer.FullName);
+        }
+        [Fact]
+        public void Fullname_FirstNameNull_EqualLastName()
+        {
+            // arrange
+            var customer = new Customer
+            {
+                CustomerId = 1,
+                LastName = "Atef",
+            };
+
+            // act
+            var acutal = "Atef";
+
+            // assert
+            Assert.Equal(acutal, customer.FullName);
+        }
+    }
+}
